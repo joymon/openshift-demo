a822913d082d95afaa413e724f6124ff259feab2
Joy George Kunjikkuru <joymon@gmail.com>
2018-02-26 20:54:32 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var express = require('express'),
     app     = express(),
     morgan  = require('morgan');
 Object.assign=require('object-assign')
-
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
 
